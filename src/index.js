8ab32a2e696879a679a3c7020547b6eb911627df
Mico52000 <micoairforce12345@gmail.com>
2020-10-07 19:48:27 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App.js'
 import * as serviceWorker from './serviceWorker';
-import {robots} from "./robots.js";
 import 'tachyons';
 
 ReactDOM.render(
